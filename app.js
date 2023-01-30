6a24f8de25eee89d42c0c0210dcb839cd2f73155
ehsc <esucasacac@unsa.edu.pe>
2023-01-29 23:13:21 -0500

    boton volumen añadido

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const $pause = document.querySelector('#pause')
 const $forward = document.querySelector('#forward')
 const $backward = document.querySelector('#backward')
 const $progress = document.querySelector('#progress')
+const $volumeOn = document.querySelector('#volumeOn')
+const $volumeOff = document.querySelector('#volumeOff')
 
 $play.addEventListener("click",handlePlay);
 $pause.addEventListener("click",handlePause);
@@ -11,7 +13,9 @@ $forward.addEventListener("click",handleForward);
 $backward.addEventListener("click",handleBackward);
 $video.addEventListener("loadedmetadata", handleLoaded);
 $video.addEventListener("timeupdate", handleTimeUpdate);
-
+$progress.addEventListener('input',handleInput);
+$volumeOn.addEventListener("click",handleSonido);
+$volumeOff.addEventListener("click",handleNoSonido);
 
 function handlePlay() {
     $video.play();
@@ -38,11 +42,21 @@ function handleLoaded() {
 }
 
 function handleTimeUpdate() {
-    $progress.value = video.currentTime
+    $progress.value = video.currentTime;
 }
 
-$progress.addEventListener('input',handleInput);
-
 function handleInput(){
-    $video.currentTime = $progress.value
+    $video.currentTime = $progress.value;
+}
+
+function handleSonido() {
+    $video.volume = 1.0;
+    $volumeOn.hidden = true;
+    $volumeOff.hidden = false;
+}
+
+function handleNoSonido() {
+    $video.volume = 0.0;
+    $volumeOff.hidden = true;
+    $volumeOn.hidden = false;
 }
